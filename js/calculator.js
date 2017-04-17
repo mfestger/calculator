c71d48ff28f781af3934e964375bcd3e421486a6
Robin Ury <r.ury@tuta.io>
2017-04-17 17:26:54 -0500

    Changes to calculator

--- js/calculator.js
+++ js/calculator.js
@@ -7,35 +7,73 @@
 var buttons = document.querySelectorAll('input[type="button')
 buttons.forEach( (button) => {
   button.addEventListener('click', function() {
-    var value = button.val
+    var val = button.value
     var inputs = document.querySelectorAll('input[type="text"]');
     var phase;
-    //Determine phase
-    if (inputs[0].value !== '') { phase = 1 }
-    if (inputs[1].value !== '') { phase = 2 }
-    if (inputs[2].value !== '') { phase = 3 }
-    //console.log(`Currently in phase: ${phase}`)
-
-    switch(val) {
-    case '+':
-      if(phase == 2)
-      break;
-    case '-':
-      //subtract
-      break;
-    case 'x':
-      //multiply
-      break;
-    case '/':
-      //Divide
-      break;
-    case '=':
-      //Compute and re-assign input1
-      break;
-    case 'C':
-      //Reset current input
-      //Halt and catch fire if input empty
-      break;
+    function evaluate() {
+      var lho = parseInt(inputs[0].value)
+      var op = inputs[1].value
+      var rho = parseInt(inputs[2].value)
+      var result;
+      switch(op) {
+        case '+':
+          result =  lho + rho
+          break;
+        case '-':
+          result =  lho - rho
+          break;
+        case 'x':
+          result =  lho * rho
+          break;
+        case '/':
+          result =  lho / rho
+          break;
+      }
+      console.log(result)
+      return result
     }
+    //Determine phase
+    if (inputs[1].value == '' && inputs[2].value == '') { phase = 1 } 
+    else if (inputs[2] == '') { phase = 2 }
+    else { phase = 3 }
+    console.log(`Currently in phase: ${phase}`)
+    if ( !(isNaN(parseInt(val))) && phase !== 2) {
+      inputs[phase-1].value += val
+    }
+    else {
+      switch(val) {
+      case '+':
+        if (inputs[0].value !== '')
+          inputs[1].value = val
+        break;
+      case '-':
+        //subtract
+        if (inputs[0].value !== '')
+          inputs[1].value = val
+        //if(phase == 3)
+        break;
+      case 'x':
+        //multiply
+        if (inputs[0].value !== '')
+          inputs[1].value = val
+        //if(phase == 3)
+        break;
+      case '/':
+        //Divide
+        if (inputs[0].value !== '')
+          inputs[1].value = val
+        //if(phase == 3)
+        break;
+      case '=':
+        alert(evaluate())
+        //Compute and re-assign input1
+          
+        break;
+      case 'C':
+        inputs.forEach( (i) => { i.value = '' })
+        break;
+      }
+    }
+
   })
 })
