f99c636708ff9e4ee9fe178333768665ba8f21ac
Robin Ury <r.ury@tuta.io>
2017-04-17 19:24:45 -0500

    Added normalize css library. General styling adjustments. Renamed css

--- js/calculator.js
+++ js/calculator.js
@@ -2,78 +2,94 @@
  * Created by michaelfestger on 4/17/17.
  */
 
-"use strict";
+'use strict'
+
 
 var buttons = document.querySelectorAll('input[type="button')
-buttons.forEach( (button) => {
-  button.addEventListener('click', function() {
+buttons.forEach((button) => {
+  button.addEventListener('click', function () {
     var val = button.value
-    var inputs = document.querySelectorAll('input[type="text"]');
-    var phase;
-    function evaluate() {
+    var inputs = document.querySelectorAll('input[type="text"]')
+    var phase
+    function clear () {
+      inputs.forEach( (i) => { i.value = '' })
+    }
+    function evaluate () {
       var lho = parseInt(inputs[0].value)
       var op = inputs[1].value
       var rho = parseInt(inputs[2].value)
-      var result;
-      switch(op) {
+      var result
+      switch (op) {
         case '+':
-          result =  lho + rho
-          break;
+          result = lho + rho
+          break
         case '-':
-          result =  lho - rho
-          break;
+          result = lho - rho
+          break
         case 'x':
-          result =  lho * rho
-          break;
+          result = lho * rho
+          break
         case '/':
-          result =  lho / rho
-          break;
+          result = lho / rho
+          break
       }
       console.log(result)
+      clear()
       return result
     }
-    //Determine phase
-    if (inputs[1].value == '' && inputs[2].value == '') { phase = 1 } 
-    else if (inputs[2] == '') { phase = 2 }
-    else { phase = 3 }
+    // Determine phase
+    if (inputs[1].value) { phase = 2} // Pending evaluation
+    else { phase = 1 } //Pending LHO
     console.log(`Currently in phase: ${phase}`)
-    if ( !(isNaN(parseInt(val))) && phase !== 2) {
-      inputs[phase-1].value += val
-    }
-    else {
-      switch(val) {
-      case '+':
-        if (inputs[0].value !== '')
-          inputs[1].value = val
-        break;
-      case '-':
-        //subtract
-        if (inputs[0].value !== '')
-          inputs[1].value = val
-        //if(phase == 3)
-        break;
-      case 'x':
-        //multiply
-        if (inputs[0].value !== '')
-          inputs[1].value = val
-        //if(phase == 3)
-        break;
-      case '/':
-        //Divide
-        if (inputs[0].value !== '')
-          inputs[1].value = val
-        //if(phase == 3)
-        break;
-      case '=':
-        alert(evaluate())
-        //Compute and re-assign input1
-          
-        break;
-      case 'C':
-        inputs.forEach( (i) => { i.value = '' })
-        break;
+
+    if (isNaN(parseInt(val)) == false) {
+      if(phase == 1)
+        inputs[0].value += val
+      else
+        inputs[2].value += val
+    }else {
+      switch (val) {
+        case '+':
+          if (inputs[0].value !== '')
+            inputs[1].value = val
+          break
+        case '-':
+          // subtract
+          if (inputs[0].value !== '')
+            inputs[1].value = val
+          // if(phase == 3)
+          break
+        case 'x':
+          // multiply
+          if (inputs[0].value !== '')
+            inputs[1].value = val
+          // if(phase == 3)
+          break
+        case '/':
+          // Divide
+          if (inputs[0].value !== '')
+            inputs[1].value = val
+          // if(phase == 3)
+          break
+        case '=':
+          inputs[0].value = evaluate()
+          // TO-DO possibly add flag for input0 after eval
+
+          break
+        case 'C':
+          clear()
+          break
       }
     }
-
   })
 })
+
+function shuffle(a) {
+    for (let i = a.length; i; i--) {
+        let j = Math.floor(Math.random() * i);
+        [a[i - 1], a[j]] = [a[j], a[i - 1]];
+    }
+}
+var wallpapers = [
+'https://i.redd.it/dtinkdtc37qy.jpg',
+]
