b3f6142d48b2d5d1b3d31c1f30f5c17ed79e88c7
michaelfestger <festger@gmail.com>
2017-04-19 09:56:48 -0500

    made adjustments to logic and css.

--- js/calculator.js
+++ js/calculator.js
@@ -1,79 +1,138 @@
 /**
- * Created by michaelfestger on 4/17/17.
+ * Created by michaelfestger on 4/18/17.
  */
+'use strict'
 
-"use strict";
-
-var buttons = document.querySelectorAll('input[type="button')
-buttons.forEach( (button) => {
-  button.addEventListener('click', function() {
+function Calculator (button) {
     var val = button.value
     var inputs = document.querySelectorAll('input[type="text"]');
-    var phase;
-    function evaluate() {
-      var lho = parseInt(inputs[0].value)
-      var op = inputs[1].value
-      var rho = parseInt(inputs[2].value)
-      var result;
-      switch(op) {
-        case '+':
-          result =  lho + rho
-          break;
-        case '-':
-          result =  lho - rho
-          break;
-        case 'x':
-          result =  lho * rho
-          break;
-        case '/':
-          result =  lho / rho
-          break;
-      }
-      console.log(result)
-      return result
-    }
-    //Determine phase
-    if (inputs[1].value == '' && inputs[2].value == '') { phase = 1 } 
-    else if (inputs[2] == '') { phase = 2 }
-    else { phase = 3 }
-    console.log(`Currently in phase: ${phase}`)
-    if ( !(isNaN(parseInt(val))) && phase !== 2) {
-      inputs[phase-1].value += val
-    }
-    else {
-      switch(val) {
-      case '+':
-        if (inputs[0].value !== '')
-          inputs[1].value = val
-        break;
-      case '-':
-        //subtract
-        if (inputs[0].value !== '')
-          inputs[1].value = val
-        //if(phase == 3)
-        break;
-      case 'x':
-        //multiply
-        if (inputs[0].value !== '')
-          inputs[1].value = val
-        //if(phase == 3)
-        break;
-      case '/':
-        //Divide
-        if (inputs[0].value !== '')
-          inputs[1].value = val
-        //if(phase == 3)
-        break;
-      case '=':
-        alert(evaluate())
-        //Compute and re-assign input1
-          
-        break;
-      case 'C':
-        inputs.forEach( (i) => { i.value = '' })
-        break;
-      }
+    console.log(`Val : ${val}`);
+
+    function clear () {
+        inputs.forEach(i => {
+            i.value = ''
+    })
     }
 
-  })
+    function evaluate () {
+        var lho = parseInt(inputs[0].value);
+        var opr = inputs[1].value;
+        var rho = parseInt(inputs[2].value);
+        var result;
+        switch (opr) {
+            case '+':
+                result = lho + rho;
+                break;
+            case '-':
+                result = lho - rho;
+                break;
+            case '×':
+                result = lho * rho;
+                break;
+            case '÷':
+                result = lho / rho;
+                break
+        }
+        inputs[0].setAttribute('cleared', ''); // Prevents appending to i[0] return
+        clear()
+        return result
+    }
+    // Determine phase of calculation
+    var phase = {
+        0: !(inputs[0].value), // Awaiting LHO
+        1: !!(inputs[1].value), // Awaiting RHO
+        2: !!(inputs[2].value) // Both operands are ready
+    }
+
+    if (!isNaN(val)) { // We can only omit casting val becasue isNaN('0') => false
+        if (!phase[1]) {
+            inputs[0].value = inputs[0].hasAttribute('cleared') ? val : inputs[0].value + val
+        } else {
+            inputs[2].value += val
+        }
+        inputs[0].removeAttribute('cleared') // Job's done
+    } else if (!phase[0]) {
+        switch (val) {
+            case '=':
+                if (phase[2]) { inputs[0].value = evaluate() }
+                break
+            case 'c':
+                console.log("Clear!!") // DEBUG
+                clear()
+                break
+            case 'Backspace':
+                if (!phase[1]) {
+                    inputs[0].value = inputs[0].value.slice(0, inputs[0].value.length - 1)
+                } else {
+                    inputs[2].value = inputs[2].value.slice(0, inputs[2].value.length - 1)
+                    if (inputs[2].value.length === 0) { inputs[1].value = '' }
+                }
+                break
+            default: // We can infer that we are assigning some operator
+                if (phase[2]) {
+                    inputs[0].value = evaluate()
+                    inputs[1] = val
+                } else {}
+                inputs[1].value = val
+        }
+    }
+}
+
+var buttons = document.querySelectorAll('input[type="button')
+buttons.forEach(button => {
+    button.addEventListener('click', () => Calculator(button))
 })
+
+window.addEventListener('keydown', keyHandler)
+function keyHandler (event) {
+    console.log(event.key) // DEBUG
+    try {
+        var key = document.querySelector(`input[value="${event.key}"`) || document.querySelector(`input[op="${event.key}"]`)
+        key.click()
+    } catch (e) {
+        if (e instanceof TypeError) { /* WE R RLY LZY */ }
+    }
+    console.log(key) // DEBUG
+}
+
+
+// document.write('<style type="text/css"> body{background-image:url("img/' + (1 + Math.floor(Math.random() * 14)) + '.jpg") ;}</style>');
+
+var imageArray = [
+    'qhufOoS',
+    'Ny8GxHb',
+    'rVK9Ms3',
+    'Yp8mk6g',
+    'rAjitMK',
+    'RYKP86x',
+    'l41y9I8',
+    'XVvmmc3',
+    '5laWd80',
+    '3S8AMrZ',
+    'EqiVIup',
+    'g85DJ6Z'
+];
+
+
+
+function setWallpaper(){
+    var backgroundImages = shuffle(imageArray);
+    var newWallPaper = backgroundImages[0];
+    document.body.setAttribute("style", "background-image: url(http://i.imgur.com/" + newWallPaper + ".jpg");
+    function shuffle (a) {
+        for (let i = a.length; i; i--) {
+            let j = Math.floor(Math.random() * i)
+            ;[a[i - 1], a[j]] = [a[j], a[i - 1]]
+        }
+        return a;
+    }
+}
+
+var shuffleButton = document.querySelector("#wallpaperGenerator");
+
+shuffleButton.addEventListener('click', setWallpaper);
+
+function clickey() {
+
+
+}
